50a009efc1315b841c62eefc49a9f6b974a9b8fc
Jaime Chochi rieba <macjaime@MBP-de-Jaime.home>
2023-07-27 23:04:13 +0200

    calcula el precio de hotel por noche

---
+++ index.js
@@ -0,0 +1,70 @@
+const body = document.querySelector("body");
+
+function formulario() {
+  const form = document.createElement("form");
+  form.id = "formulario";
+  form.innerHTML = `
+    <label for="city">Ciudad:</label>
+
+<select id="city">
+  <option value="Barcelona">Barcelona</option>
+  <option value="Madrid">Madrid</option>
+  <option value="Sevilla">Sevilla</option>
+  <option value="Valencia">Valencia</option>
+</select>
+    <label for="name">Nùmero de noches:</label>
+    <input type="number" id="hotel" name="name">
+    <label for="name">Número de días de alquiler del coche:</label>
+    <input type="text" id="coche" name="name">
+    <button type="submit">Calcular coste</button>
+    
+    `;
+  body.appendChild(form);
+}
+formulario();
+
+
+
+const inputCoche = document.querySelector("#coche");
+const submit = document.querySelector("button");
+const nocheDeHotel = 140;
+
+function costeHotel() {
+    submit.addEventListener("click", (event)=>{
+        event.preventDefault();
+        const inputHotel = document.querySelector("#hotel");
+        const inputValue = inputHotel.value;
+        const costeHotel = inputValue * nocheDeHotel;
+        console.log(costeHotel);
+    })
+    
+}
+costeHotel()
+
+const precioAvion = {
+Barcelona: 90,
+Madrid: 90,
+Sevilla: 50,
+Valencia: 40,
+};
+
+function costeAvion (ciudad, noches) {
+    submit.addEventListener("click", (event)=>{
+        event.preventDefault();
+        const inputCiudad = document.querySelector("#city");
+        const inputCiudadValue = inputCiudad.value;
+
+
+    })
+    
+}
+costeAvion();
+
+
+
+
+
+
+
+
+
