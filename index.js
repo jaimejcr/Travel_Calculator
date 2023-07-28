e0d59e4a926f59f5ffbf1c4174a033bfa42df19b
Jaime Chochi rieba <macjaime@MacBook-Pro-de-Jaime.local>
2023-07-28 23:28:57 +0200

    Done

--- index.js
+++ index.js
@@ -1,125 +1,106 @@
 const body = document.querySelector("body");
-
+const form = document.createElement("form");
+const precioHotelNoche = 140;
 
 function formulario() {
-  const form = document.createElement("form");
   form.id = "formulario";
   form.innerHTML = `
-    <label for="city">Ciudad:</label>
+    <label for="city"> Ciudad: </label>
     <select id="city">
-      <option value="Barcelona">Barcelona</option>
-      <option value="Madrid">Madrid</option>
-      <option value="Sevilla">Sevilla</option>
-      <option value="Valencia">Valencia</option>
+        <option value="Barcelona"> Barcelona </option>
+        <option value="Madrid"> Madrid </option>
+        <option value="Sevilla"> Sevilla </option>
+        <option value="Valencia"> Valencia </option>
     </select>
-    <label for="name">Número de noches:</label>
+    <label for="hotel"> Número de noches: </label>
     <input type="number" id="hotel" name="name">
-    <label for="name">Número de días de alquiler del coche:</label>
+    <label for="coche"> Número de días de alquiler del coche: </label>
     <input type="number" id="coche" name="name">
-    <button type="submit">Calcular coste</button>
-  `;
-  body.appendChild(form);
-}
+    <button type="submit"> Calcular coste </button>
+    `;
+  const resultDom = document.createElement("div");
+  resultDom.id = "resultado";
+  resultDom.style.backgroundColor = "#f0f0f0";
+  resultDom.style.padding = "10px";
+  resultDom.style.marginTop = "20px";
+  resultDom.style.border = "2px double black";
 
-// -------------------
+  body.appendChild(form);
+  body.appendChild(resultDom);
+}
+formulario();
 
 function costeHotel() {
-  const inputHotel = document.querySelector("#hotel");
-  const submit = document.querySelector("button");
-  const nocheDeHotel = 140;
-
-  submit.addEventListener("click", (event) => {
-    event.preventDefault();
-    const inputValue = inputHotel.value;
-    const costeHotel = inputValue * nocheDeHotel;
-    console.log(costeHotel);
-  });
+  const numNochesHotel = parseInt(document.querySelector("#hotel").value, 10);
+  const precioHotelPorDiasSeleccionados = numNochesHotel * precioHotelNoche;
+  //PrecioHotelPor.. precio calculado por dias de estancia
+  console.log(
+    `Precio calculado por ${numNochesHotel} días de estancia: ${precioHotelPorDiasSeleccionados}€`
+  );
+  return precioHotelPorDiasSeleccionados;
 }
 
-// -----------------------------------
-
-
-function costeAvion() {
+function costeAvion(ciudad, numNoches) {
   const precioAvion = {
     Barcelona: 90,
     Madrid: 90,
     Sevilla: 50,
     Valencia: 40,
   };
-  const form = document.querySelector("#formulario");
 
-  
-    const cityInput = document.querySelector("#city");
-    let cityValue = cityInput.value;
-    const inputHotel = document.querySelector("#hotel");
-    let inputValue = inputHotel.value;
-    const numNoches = parseInt(inputValue, 10);
-    const costePorNoches = precioAvion[cityValue] * numNoches;
+  const costePorNoches = precioAvion[ciudad] * numNoches;
+  console.log(
+    `Coste del viaje a ${ciudad} por ${numNoches} noches: ${costePorNoches}€`
+  );
 
-    console.log(`Coste del viaje a ${cityValue} por ${numNoches} noches: ${costePorNoches}€`);
-    if (numNoches > 3) {
-      const diezPorCiento = costePorNoches * 0.10;
-      const precioConDescuento = costePorNoches - diezPorCiento;
-      console.log(`El 10% de ${costePorNoches} euros es: ${diezPorCiento} euros`);
-      console.log(precioConDescuento);
-    }
-    return costePorNoches
+  if (numNoches > 3) {
+    const calculoDiezPorciento = costePorNoches * 0.1;
+    // precioDescuento, es el precio con el descuento ya hecho
+    const precioDescuento = costePorNoches - calculoDiezPorciento;
+    console.log(precioDescuento);
+    console.log(
+      `El 10% de ${costePorNoches} euros es: ${calculoDiezPorciento} euros`
+    );
   }
-// -------------------------------
 
-
-
+  return costePorNoches;
+}
 
 function costeCoche(numDias) {
   const costePorDia = 40;
-  let costeTotal = costePorDia * numDias;
-
-  if (numDias >= 3) {
-    costeTotal -= 20;
-  }
+  let aPagarPorCoche = costePorDia * numDias;
 
   if (numDias >= 7) {
-    costeTotal -= 50;
+    aPagarPorCoche -= 50;
+    console.log("descuento de 50€");
+  } else if (numDias >= 3) {
+    aPagarPorCoche -= 20;
+    console.log("descuento de 20€");
   }
 
-  console.log(`Coste del alquiler del coche por ${numDias} días: ${costeTotal}€`);
-  return costeTotal;
+  console.log(
+    `Coste del alquiler del coche por ${numDias} días: ${aPagarPorCoche}€`
+  );
+  return aPagarPorCoche;
 }
 
 function calcularCoste() {
-  const inputCoche = document.querySelector("#coche");
-  const numDiasAlquilerCoche = parseInt(inputCoche.value, 10);
-  const costeHotel = 140 * parseInt(document.querySelector("#hotel").value, 10);
-  const costeAvionTotal = costeAvion();
-  const costeCocheTotal = costeCoche(numDiasAlquilerCoche);
-  
-  const costeTotal = costeHotel + costeAvionTotal + costeCocheTotal;
+  const numNochesHotel = parseInt(document.querySelector("#hotel").value, 10);
+  const ciudadSeleccionada = document.querySelector("#city").value;
+  const numDiasConCoche = parseInt(document.querySelector("#coche").value, 10);
 
-  console.log(`Coste total del viaje: ${costeTotal}€`);
-
-  return costeTotal
+  // calcularCoste()
+  const costoHotel = costeHotel();
+  const costoAvion = costeAvion(ciudadSeleccionada, numNochesHotel);
+  const costoCoche = costeCoche(numDiasConCoche);
+  const resultado = document.querySelector("#resultado");
+  const totalAPagar = costoHotel + costoAvion + costoCoche;
+  resultado.innerHTML = `
+  <h2> Coste hotel : ${costoHotel}€ - Coste vuelo :${costoAvion}€ - Coste coche : ${costoCoche} - Total : ${totalAPagar} </h2>
+  `;
 }
 
-formulario();
-
-
-
-const form = document.querySelector("#formulario");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const resultDom = document.createElement("div");
-  const costeTotalCalculado = calcularCoste();
-
-  resultDom.innerHTML = `
-    <h2>Coste: ${costeTotalCalculado}€</h2>
-  `;
-  resultDom.style.backgroundColor = "#f0f0f0";
-  resultDom.style.padding = "10px";
-  resultDom.style.marginTop = "20px";
-  body.appendChild(resultDom);
+  calcularCoste();
 });
-
-
-
-
-
